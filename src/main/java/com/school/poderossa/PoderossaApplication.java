81f1b4f480d2efe1b08c1abd8fba279cb33a0d67
masaki nakamura <gmk4n0n@gmail.com>
2022-01-14 01:07:11 +0900

    first commit

---
+++ src/main/java/com/school/poderossa/PoderossaApplication.java
@@ -0,0 +1,13 @@
+package com.school.poderossa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PoderossaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PoderossaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/school/poderossa/PoderossaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.school.poderossa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PoderossaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
